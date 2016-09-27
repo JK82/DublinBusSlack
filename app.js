d46362e8478fdd3b45e35315d9ab8f409184a30b
John Keane <johnkeanejnr@gmail.com>
2016-09-27 21:34:17 +0100

    adding request to bus api

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.post('/', function (req, res) {
 	axios.get('http://data.dublinked.ie/cgi-bin/rtpi/realtimebusinformation?stopid='+ busStopNumber +'&format=json')
 	  .then(function (response) {
 	  	console.log('HI JOHN')
-	  	console.log(response.results[0])
+	  	console.log(response)
 	    res.send(simpleStringify(response.results))
 	  })
 	  .catch(function (error) {
