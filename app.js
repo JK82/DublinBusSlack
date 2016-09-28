e3b647e47492f86057da1c5e994cf47522aafac2
John Keane <johnkeanejnr@gmail.com>
2016-09-28 19:14:41 +0100

    adding message construction

--- app.js
+++ app.js
@@ -10,11 +10,30 @@ app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3001!');
 });
 
+'//'
+
 app.post('/', function (req, res) {
 	var busStopNumber = req.body.text;
-	axios.get('http://data.dublinked.ie/cgi-bin/rtpi/realtimebusinformation?stopid='+ busStopNumber +'&format=json')
+	var busStopName = '';
+	var busStopEmoji = ':busstop:';
+	var busEmoji = 'bus';
+	var messageForSlack = '';
+
+	axios.get('https://data.dublinked.ie/cgi-bin/rtpi/busstopinformation?stopid='+ busStopNumber +'&format=json')
 	  .then(function (response) {
-	    res.send(response.data.results[0].origin)
+	  		busStopName = response.data.results[0].shortname;
+	  		axios.get('http://data.dublinked.ie/cgi-bin/rtpi/realtimebusinformation?stopid='+ busStopNumber +'&format=json')
+				.then(function (response) {
+					messageForSlack = 'Heres the buses due at ' + busStopEmoji + ' ' + busStopName + '/n';
+					response.data.results.map(function(bus){
+						messageForSlack += busEmoji + ' #' + bus.route + ' in ' + bus.departureduetime + ' minutes';
+					})
+					res.send(messageForSlack);
+				})
+				.catch(function (error) {
+					console.log(error);
+				});
+	    
 	  })
 	  .catch(function (error) {
 	    console.log(error);
