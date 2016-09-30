8e1e67972f587c43650c9c3ebc379c118606f57a
John Keane <johnkeanejnr@gmail.com>
2016-09-30 19:56:26 +0100

    temp from F to C

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.post('/', function (req, res) {
 				  			''
 				  	}
 				  	weather += ' ' + response.data.currently.summary;
-				  	weather += ' :thermometer:' + Math.round(((response.data.currently.apparentTemperature - 32) * .5556))  + ' - <https://darksky.net|Powered By Dark Sky> ';
+				  	weather += ' :thermometer:' + Math.round(((response.data.currently.apparentTemperature - 32) * .5556))  + '°C - <https://darksky.net|Powered By Dark Sky> ';
 				    axios.get('http://data.dublinked.ie/cgi-bin/rtpi/realtimebusinformation?stopid='+ busStopNumber +'&format=json')
 							.then(function (response) {
 								messageForSlack = "Hey " + req.body.user_name + " Here's the buses due at " + busStopEmoji + ' ' + busStopName + '\n';
