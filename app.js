e0c48c42e2e1a0978734fce7a72d225a7e35ae9f
John Keane <johnkeanejnr@gmail.com>
2016-10-18 21:27:33 +0100

    adding auth to post request

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ app.get('/auth', function (req, res) {
           params: {
             client_id: '84539294599.84589472165',
             client_secret:'7165aa57021d6e65d5291c5eb358486d',
-            code:req.query.code
+            code:req.query.code,
+            redirect_url:'https://pacific-springs-98505.herokuapp.com/good'
           }
         })
       .then(function (response) {
